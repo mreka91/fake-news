4dfd67a5e30b26ec6253fea8283a8568ff82c358
Reka Madarasz <mreka91@gmail.com>
2020-10-19 10:50:34 +0200

    articles array

--- data.php
+++ data.php
@@ -4,3 +4,85 @@ declare(strict_types=1);
 
 // This is the file where you can keep your data arrays such as articles and
 // authors.
+
+$articles = [
+    [
+        'title' => 'Lorem title',
+        'content' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat accusamus totam id exercitationem, adipisci nisi ipsam impedit odio beatae distinctio aperiam, delectus iusto laudantium quasi est culpa quidem enim. Blanditiis?',
+        'author' => 'Reka',
+        'published_date' => '2020-10-19',
+        'likes' => '10'
+    ],
+
+    [
+        'title' => 'Lorem title',
+        'content' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat accusamus totam id exercitationem, adipisci nisi ipsam impedit odio beatae distinctio aperiam, delectus iusto laudantium quasi est culpa quidem enim. Blanditiis?',
+        'author' => 'Reka',
+        'published_date' => '2020-10-18',
+        'likes' => '20'
+    ],
+
+    [
+        'title' => 'Lorem title',
+        'content' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat accusamus totam id exercitationem, adipisci nisi ipsam impedit odio beatae distinctio aperiam, delectus iusto laudantium quasi est culpa quidem enim. Blanditiis?',
+        'author' => 'Peter',
+        'published_date' => '2020-10-17',
+        'likes' => '70'
+    ],
+
+    [
+        'title' => 'Lorem title',
+        'content' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat accusamus totam id exercitationem, adipisci nisi ipsam impedit odio beatae distinctio aperiam, delectus iusto laudantium quasi est culpa quidem enim. Blanditiis?',
+        'author' => 'Peter',
+        'published_date' => '2020-10-17',
+        'likes' => '50'
+    ],
+
+    [
+        'title' => 'Lorem title',
+        'content' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat accusamus totam id exercitationem, adipisci nisi ipsam impedit odio beatae distinctio aperiam, delectus iusto laudantium quasi est culpa quidem enim. Blanditiis?',
+        'author' => 'Lisa',
+        'published_date' => '2020-10-16',
+        'likes' => '20'
+    ],
+
+    [
+        'title' => 'Lorem title',
+        'content' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat accusamus totam id exercitationem, adipisci nisi ipsam impedit odio beatae distinctio aperiam, delectus iusto laudantium quasi est culpa quidem enim. Blanditiis?',
+        'author' => 'Lisa',
+        'published_date' => '2020-10-15',
+        'likes' => '150'
+    ],
+
+    [
+        'title' => 'Lorem title',
+        'content' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat accusamus totam id exercitationem, adipisci nisi ipsam impedit odio beatae distinctio aperiam, delectus iusto laudantium quasi est culpa quidem enim. Blanditiis?',
+        'author' => 'Jeff',
+        'published_date' => '2020-10-14',
+        'likes' => '50'
+    ],
+
+    [
+        'title' => 'Lorem title',
+        'content' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat accusamus totam id exercitationem, adipisci nisi ipsam impedit odio beatae distinctio aperiam, delectus iusto laudantium quasi est culpa quidem enim. Blanditiis?',
+        'author' => 'Jeff',
+        'published_date' => '2020-10-13',
+        'likes' => '50'
+    ],
+
+    [
+        'title' => 'Lorem title',
+        'content' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat accusamus totam id exercitationem, adipisci nisi ipsam impedit odio beatae distinctio aperiam, delectus iusto laudantium quasi est culpa quidem enim. Blanditiis?',
+        'author' => 'Bob',
+        'published_date' => '2020-10-12',
+        'likes' => '70'
+    ],
+
+    [
+        'title' => 'Lorem title',
+        'content' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat accusamus totam id exercitationem, adipisci nisi ipsam impedit odio beatae distinctio aperiam, delectus iusto laudantium quasi est culpa quidem enim. Blanditiis?',
+        'author' => 'Bob',
+        'published_date' => '2020-10-10',
+        'likes' => '60'
+    ]
+];
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-
+require __DIR__ . '/data.php';
+require __DIR__ . '/functions.php';
 // This is the file where you can keep your HTML markup. We should always try to
 // keep us much logic out of the HTML as possible. Put the PHP logic in the top
 // of the files containing HTML or even better; in another PHP file altogether.
@@ -12,10 +13,31 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title></title>
+    <title>Fake Neews For You</title>
 </head>
 
 <body>
 </body>
+<div>
+    <?php foreach ($articles as $article) : ?>
+        <h1>
+            <?= $article['title']; ?>
+        </h1>
+        <p>
+            <?= $article['content']; ?>
+        </p>
+        <p>Author:
+            <?= $article['author']; ?>
+        </p>
+        <p>Published at:
+            <?= $article['published_date']; ?>
+        </p>
+        <p>Likes:
+            <?= $article['likes']; ?>
+        </p>
+
+    <?php endforeach ?>
+
+</div>
 
 </html>
