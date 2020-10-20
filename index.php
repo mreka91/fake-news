ddc9e020393ee9ad4f9c3b734d1142dc9ce409df
Reka Madarasz <mreka91@gmail.com>
2020-10-20 13:59:47 +0200

    added posts to index

--- index.php
+++ index.php
@@ -16,6 +16,8 @@ require __DIR__ . '/functions.php';
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <!-- Add icon library -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- google font-->
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap" rel="stylesheet">
     <!-- own css-->
     <link rel=" stylesheet" href="style.css">
 
@@ -23,41 +25,45 @@ require __DIR__ . '/functions.php';
 </head>
 
 <body>
-    <!-- Try grid -->
-    <header>
-        <h1>The Office Gossip!</h1>
-        <span>Brought to you by the best from Dunder Mifflin</span>
-
+    <!-- HEADER-->
+    <header class="hero-image">
+        <div class="hero-text">
+            <h1>The Office Gossip!</h1>
+            <span>Only the truth, brought to you by the best from Dunder Mifflin</span>
+        </div>
     </header>
 
 
-    <main class="main">
-        <article>
+    <!-- POSTS-->
+    <main class="container">
+        <?php foreach ($articles as $article) : ?>
+            <div class="row">
+                <div class="col-lg-8 col-md-10 mx-auto">
+                    <div class="post-preview">
+                        <a href="post.html">
+                            <h2 class="post-title">
+                                <?= $article['title']; ?>
+                            </h2>
+                        </a>
+                        <p class="post-subtitle">
+                            <?= $article['content']; ?>
+                        </p>
+                        <p class="post-meta">Posted by
+                            <a href="#"><?= $article['author']; ?></a>
+                            <?= $article['published_date']; ?></p>
+                        <p class="post-meta">Likes:
+                            <?= $article['likes']; ?>
+                            &#10084;
+                        </p>
+                    </div>
+                    <hr>
+                </div>
 
-            <?php foreach ($articles as $article) : ?>
-                <h2>
-                    <?= $article['title']; ?>
-                </h2>
-                <img src="<?= $article['image']; ?>" alt="">
-                <p>
-                    <?= $article['content']; ?>
-                </p>
-                <p>Author:
-                    <?= $article['author']; ?>
-                </p>
-                <p>Published at:
-                    <?= $article['published_date']; ?>
-                </p>
-                <p>Likes:
-                    <?= $article['likes']; ?>
-                    &#10084;
-                </p>
-
-            <?php endforeach ?>
-
-        </article>
+            </div>
+        <?php endforeach ?>
     </main>
 
+    <!-- FOOTER-->
 
     <footer>
         <p>For more definitely not fake news, find us on social media:</p>
@@ -66,7 +72,7 @@ require __DIR__ . '/functions.php';
             <a href="#" class="fa fa-twitter"></a>
             <a href="#" class="fa fa-linkedin"></a>
         </div>
-
+        <p>&copy; 2013 Dunder Mifflin<p>
     </footer>
 
 </body>
